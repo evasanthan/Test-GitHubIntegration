b987d4fd788d77c92a510348d4a4fd2b9ad8db6c
Vasanthan <evasanthan@gmail.com>
2018-03-02 13:31:35 +0530

    [finishes #155648402]
    Test with brackets

--- Integrate2.java
+++ Integrate2.java
@@ -2,6 +2,5 @@
 class Integrate2{
 public static void main(String args[]){
 System.out.println("Welcome to Branch Commit Test");
-  /* added the commit */
 /* Added more commit statements - One More comits */
 }
