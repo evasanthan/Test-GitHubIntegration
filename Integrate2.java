01016baaeb08d71df0e86d8041f368a8b69cdb79
Vasanthan <evasanthan@gmail.com>
2018-03-02 13:29:17 +0530

    [finishes #155648402]
    Check the option commit messages

--- Integrate2.java
+++ Integrate2.java
@@ -1,7 +1,7 @@
 /* This class basically to test the commit */
 class Integrate2{
 public static void main(String args[]){
-
+/*Sout display */
 System.out.println("Welcome to Branch Commit Test");
   /* added the commit */
 /* Added more commit statements - One More comits */
